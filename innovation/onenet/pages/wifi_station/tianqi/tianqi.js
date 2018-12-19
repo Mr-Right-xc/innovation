698e05fc55b23462cf95747b638061b6cb5cbc9f
Mr-Right-xc <45918316+Mr-Right-xc@users.noreply.github.com>
2018-12-19 23:49:23 +0800

    Add files via upload

---
+++ innovation/onenet/pages/wifi_station/tianqi/tianqi.js
@@ -0,0 +1,234 @@
+var app=getApp()
+
+var myCharts = require("../../../utils/wxcharts.js")//引入一个绘图的插件
+var lineChart_hum = null
+var lineChart_light = null
+var lineChart_tempe = null
+var app = getApp()
+
+Page({
+  data: {
+  },
+  onPullDownRefresh: function () {
+    console.log('onPullDownRefresh', new Date())
+  },
+
+
+  //把拿到的数据转换成绘图插件需要的输入格式
+  convert: function () {
+    var pressure = [];
+    var pressure = [];
+    var light = [];
+    var tempe = [];
+
+    var length = app.globalData.light.datapoints.length
+    for (var i = 0; i < length; i++) {
+      categories.push(app.globalData.humidity.datapoints[i].at.slice(11,19));
+      humidity.push(app.globalData.humidity.datapoints[i].value);
+      light.push(app.globalData.light.datapoints[i].value);
+      tempe.push(app.globalData.temperature.datapoints[i].value);
+    }
+    return {
+      pressure: pressure,
+   
+    }
+  },
+  if(pressure <sta) {
+    wx.showModal({
+      title: '警报！',
+      content: 'c101状态异常',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+
+  onLoad: function () {
+    var wheatherData = this.convert();
+    
+    //得到屏幕宽度
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    var wheatherData = this.convert();
+
+    //新建湿度图表
+    lineChart_hum = new myCharts({
+      canvasId: 'humidity',
+      type: 'line',
+      categories: wheatherData.categories,
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: 'humidity',
+        data: wheatherData.humidity,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: 'humidity (%)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 55
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+
+  //新建光照强度图表
+    lineChart_light = new myCharts({
+      canvasId: 'light',
+      type: 'line',
+      categories: wheatherData.categories,
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: 'light',
+        data: wheatherData.light,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: 'light (lux)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 190
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+
+    //新建温度图表
+    lineChart_tempe = new myCharts({
+      canvasId: 'tempe',
+      type: 'line',
+      categories: wheatherData.categories,
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: 'temperature',
+        data: wheatherData.tempe,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: 'temperature (摄氏度)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 24
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+  },
+
+  
+})
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+    
+    
+    
+    
+    
+    //bool:app.bool1
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
